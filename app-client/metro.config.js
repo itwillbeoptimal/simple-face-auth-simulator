c47af54656031c334732d10014fbc3de89d0766c
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-11-09 23:20:41 +0900

    chore: Metro bundler 설정 추가
    - 모노레포 watchFolders 설정 및 SVG transformer 설정 추가

--- app-client/metro.config.js
+++ app-client/metro.config.js
@@ -2,13 +2,18 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 const path = require('path');
 
 const config = {
-    projectRoot: __dirname,
-    watchFolders: [path.resolve(__dirname, '..')],
-    resolver: {
-        nodeModulesPaths: [
-            path.resolve(__dirname, '..', 'node_modules'),
-        ],
-    },
+  projectRoot: __dirname,
+  watchFolders: [path.resolve(__dirname, '..')],
+  resolver: {
+    nodeModulesPaths: [
+      path.resolve(__dirname, '..', 'node_modules'),
+    ],
+    assetExts: getDefaultConfig(__dirname).resolver.assetExts.filter(ext => ext !== 'svg'),
+    sourceExts: [...getDefaultConfig(__dirname).resolver.sourceExts, 'svg'],
+  },
+  transformer: {
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+  },
 };
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
