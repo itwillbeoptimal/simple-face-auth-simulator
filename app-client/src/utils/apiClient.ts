100736b0c4bc0a5613fc4a180732a81c5a2d2e9e
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-11-11 00:59:16 +0900

    feat(common): API 클라이언트 설정

---
+++ app-client/src/utils/apiClient.ts
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import { API_BASE_URL } from '@env';
+
+export const apiClient = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+apiClient.interceptors.request.use(
+  config => {
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  },
+);
+
+apiClient.interceptors.response.use(
+  response => {
+    return response;
+  },
+  error => {
+    return Promise.reject(error);
+  },
+);
