d0d0684ef612bed5f2782d3947367e2149d1a52c
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-11-11 00:59:18 +0900

    feat(common): 앱 테마 설정

---
+++ app-client/src/styles/theme/index.ts
@@ -0,0 +1,32 @@
+const theme = {
+  COLORS: {
+    MAIN: {
+      PRIMARY: '#ea5204',
+      SECONDARY: '#fbd0b9',
+    },
+    BACKGROUND: '#18181A',
+    SURFACE: '#272833',
+    LABEL: {
+      PRIMARY: '#ffffff',
+      SECONDARY: 'rgba(255, 255, 255, 0.6)',
+      TERTIARY: 'rgba(255, 255, 255, 0.3)',
+      QUATERNARY: 'rgba(255, 255, 255, 0.18)',
+      ALERT: '#ff4048',
+      SUCCESS: '#37b24d',
+      INFO: '#4263eb',
+    },
+  },
+  FONTS: {
+    THIN: 'Pretendard-Thin',
+    EXTRA_LIGHT: 'Pretendard-ExtraLight',
+    LIGHT: 'Pretendard-Light',
+    REGULAR: 'Pretendard-Regular',
+    MEDIUM: 'Pretendard-Medium',
+    SEMI_BOLD: 'Pretendard-SemiBold',
+    BOLD: 'Pretendard-Bold',
+    EXTRA_BOLD: 'Pretendard-ExtraBold',
+    BLACK: 'Pretendard-Black',
+  },
+};
+
+export default theme;
