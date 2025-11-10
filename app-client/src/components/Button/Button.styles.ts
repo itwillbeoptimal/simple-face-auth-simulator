642d1d6faf60ca0f482bf6cba8b8b10d682de731
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-11-11 00:59:35 +0900

    style(common): Button 스타일 컴포넌트 작성

---
+++ app-client/src/components/Button/Button.styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components/native';
+import { TouchableOpacity } from 'react-native';
+import StyledText from '@/components/StyledText';
+
+export const StyledButton = styled(TouchableOpacity)`
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  padding: 16px;
+  background-color: ${props => props.theme.COLORS.MAIN.PRIMARY};
+  border-radius: 12px;
+`;
+
+export const ButtonText = styled(StyledText)`
+  font-size: 18px;
+`;
