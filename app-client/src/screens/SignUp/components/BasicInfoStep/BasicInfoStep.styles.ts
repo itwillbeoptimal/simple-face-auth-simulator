053c82deec46d9d04a8c0eafd588f446d65b69a2
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-11-11 00:59:52 +0900

    style(SignUp): 기본 정보 입력 단계 스타일 컴포넌트 작성

---
+++ app-client/src/screens/SignUp/components/BasicInfoStep/BasicInfoStep.styles.ts
@@ -0,0 +1,38 @@
+import { ScrollView, View } from 'react-native';
+import styled from 'styled-components/native';
+import StyledText from '@/components/StyledText';
+
+export const Container = styled(View)`
+  flex: 1;
+`;
+
+export const ScrollContainer = styled(ScrollView).attrs({
+  contentContainerStyle: {
+    paddingBottom: 20,
+  },
+})`
+  flex: 1;
+`;
+
+export const Form = styled(View)`
+  flex: 1;
+  gap: 12px;
+`;
+
+export const Title = styled(StyledText)`
+  font-size: 28px;
+  margin-bottom: 8px;
+  letter-spacing: -1px;
+`;
+
+export const Subtitle = styled(StyledText)`
+  font-size: 16px;
+  margin-bottom: 32px;
+  letter-spacing: -0.5px;
+`;
+
+export const FieldWrapper = styled(View)``;
+
+export const InputLabel = styled(StyledText)`
+  margin: 0 4px 8px 0;
+`;
