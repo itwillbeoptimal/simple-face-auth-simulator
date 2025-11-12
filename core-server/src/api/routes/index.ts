ce7265e4b916673c253a69faf38608f43a6c558d
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-11-12 12:44:52 +0900

    feat(routes): face 라우트 등록

--- core-server/src/api/routes/index.ts
+++ core-server/src/api/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import authRoutes from './auth.routes';
 import userRoutes from './user.routes';
+import faceRoutes from './face.routes';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
 router.use('/users', userRoutes);
+router.use('/face', faceRoutes);
 
 export default router;
