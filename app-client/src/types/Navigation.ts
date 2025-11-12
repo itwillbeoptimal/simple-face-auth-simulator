b8e9ed2698a5af168d9400679ff3f83475c2eb7b
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-11-12 12:38:35 +0900

    feat: 내비게이션 타입 정의 추가

---
+++ app-client/src/types/Navigation.ts
@@ -0,0 +1,20 @@
+import type { StackNavigationProp } from '@react-navigation/stack';
+import type { RouteProp } from '@react-navigation/native';
+import type { RNFileObject } from './RNFileObject';
+
+export type RootStackParamList = {
+  Login: undefined;
+  SignUp: {
+    capturedFace?: RNFileObject;
+  };
+  FaceCapture: {
+    returnTo: 'SignUp';
+  };
+};
+
+export type NavigationProp<T extends keyof RootStackParamList> = StackNavigationProp<
+  RootStackParamList,
+  T
+>;
+
+export type RouteProps<T extends keyof RootStackParamList> = RouteProp<RootStackParamList, T>;
