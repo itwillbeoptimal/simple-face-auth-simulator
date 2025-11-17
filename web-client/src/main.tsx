8410f054a5d614864115f1c60db8d4fc74b80b91
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-11-17 16:39:33 +0900

    feat(App): QueryClient provider 설정 추가

--- web-client/src/main.tsx
+++ web-client/src/main.tsx
@@ -1,15 +1,20 @@
 import { ThemeProvider } from '@emotion/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from '@/App';
 import GlobalStyle from '@/styles/GlobalStyle';
 import theme from '@/styles/theme';
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   </StrictMode>,
 );
