269a2710c5d31e96aa94083b9a06fa078b488d04
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-11-19 02:51:33 +0900

    feat(App): Jotai Provider 추가

--- app-client/src/App.tsx
+++ app-client/src/App.tsx
@@ -1,22 +1,26 @@
+import { Provider as JotaiProvider } from 'jotai';
 import { QueryClientProvider } from '@tanstack/react-query';
-import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
 import { ThemeProvider } from 'styled-components/native';
 import { queryClient } from '@/utils/queryClient';
+import { store } from '@/store';
 import theme from '@/styles/theme';
 import RootNavigator from '@/components/RootNavigator';
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <SafeAreaProvider>
-        <NavigationContainer>
-          <ThemeProvider theme={theme}>
-            <RootNavigator />
-          </ThemeProvider>
-        </NavigationContainer>
-      </SafeAreaProvider>
-    </QueryClientProvider>
+    <JotaiProvider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <SafeAreaProvider>
+          <NavigationContainer>
+            <ThemeProvider theme={theme}>
+              <RootNavigator />
+            </ThemeProvider>
+          </NavigationContainer>
+        </SafeAreaProvider>
+      </QueryClientProvider>
+    </JotaiProvider>
   );
 }
 
